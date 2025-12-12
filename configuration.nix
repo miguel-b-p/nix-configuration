592bfcbd962bb2965acefad1bcfe828c026e873f
Mingas <miguelpinotty@gmail.com>
2025-12-12 13:15:08 -0300

    Remove budgie

--- configuration.nix
+++ configuration.nix
@@ -14,8 +14,7 @@
     ./hardware/drives
     ./networking
     ./localization
-    #./desktop/plasma.nix
-    ./desktop/budgie.nix
+    ./desktop/plasma.nix
     ./desktop/xdg-portal.nix
     ./services/audio.nix
     ./services/printing.nix
