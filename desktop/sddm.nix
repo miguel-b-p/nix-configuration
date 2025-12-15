8aaea1827c6963f8e9d95ecaf208202a259b8984
Mingas <miguelpinotty@gmail.com>
2025-12-15 12:17:59 -0300

    background image

--- desktop/sddm.nix
+++ desktop/sddm.nix
@@ -3,24 +3,32 @@
 let
   custom-qogir-sddm = pkgs.stdenv.mkDerivation {
     name = "qogir-sddm-theme";
-    src = ./sddm/Qogir;
+    src = pkgs.fetchFromGitHub {
+      owner = "vinceliuice";
+      repo = "Qogir-kde";
+      rev = "master";
+      sha256 = "zgXwYmpD31vs2Gyg21m0MdOkwqzSn6V21Kva+nvNeVI=";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share/sddm/themes
+      cp -r sddm/Qogir $out/share/sddm/themes/Qogir
+
+      # Copy user background
+      mkdir -p $out/share/sddm/themes/Qogir/backgrounds
+      cp -r ${./background.jpg} $out/share/sddm/themes/Qogir/backgrounds/background.jpg
+    '';
   };
 in
 {
   environment.systemPackages = with pkgs; [
     custom-qogir-sddm
-    qogir-icon-theme
   ];
 
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     theme = "Qogir"; # nome do tema na pasta /share/sddm/themes
-    extraPackages = with pkgs.kdePackages; [
-      custom-qogir-sddm
-      qtsvg
-      qtmultimedia
-      qtvirtualkeyboard
-    ];
+    extraPackages = [ custom-qogir-sddm ];
   };
 }
